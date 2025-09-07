451a0a8524e3e60fa985bcf241084f2a594282d2
parvez alam <parvez.alam44@gmail.com>
2025-09-07 17:26:26 +0530

    adding new que

--- arrays/deleteOneEle.js
+++ arrays/deleteOneEle.js
@@ -1,3 +1,20 @@
 let arr = [10,20,30,40,50,60];
-let index = 3; // delet the element at index 3;
+let index = 4; // delet the element at index 3;
+
+let brr = [];
+let crr = [];
+
+for(let i=0; i<parseInt((arr.length)/2); i++){
+   brr.push(arr[i]);
+}
+
+for(let i=index+1; i<=arr.length-1; i++){
+    crr.push(arr[i]);
+}
+
+brr.pop();
+
+console.log(arr);
+console.log(brr);
+console.log(crr);
 
