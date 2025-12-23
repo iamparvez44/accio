7a3ee44846ad90486991cc88361024dda741e07f
parvez alam <parvez.alam44@gmail.com>
2025-12-23 19:28:24 +0530

    tpics added

--- map-filter-reducre/map.js
+++ map-filter-reducre/map.js
@@ -1,9 +1,42 @@
-let arr = [10,20,30,40,50];
+// let arr = [10,20,30,40,50];
 
-let x = arr.map(display);
+// let x = arr.map(display);
 
-function display(val, idx){
-    return val* val;
-}
+// function display(val, idx){
+//     return val* val;
+// }
 
-console.log(x);
+// console.log(x);
+
+
+// Convert an array of strings to uppercase.
+// ["js", "react", "node"]
+
+// let arr = ["js", "react", "node"];
+
+// let result = arr.map((val)=>{
+//     return val.toUpperCase();
+// })
+
+// console.log(result);
+// console.log(arr);
+
+
+// From an array of objects, return only names.
+
+let arr = [
+  { name: "Parvez", age: 22 },
+  { name: "Anas", age: 21 }
+];
+
+
+let result = arr.map((obj)=>({name:obj.name}));
+
+    
+
+
+
+
+
+console.log(result);
+
